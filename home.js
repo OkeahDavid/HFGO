54bfb10283ad8207287a6fbbfcd549f6528060f3
OkeahDavid <82973470+OkeahDavid@users.noreply.github.com>
2023-10-25 11:25:43 +0200

    added icon

--- home.js
+++ home.js
@@ -18,6 +18,12 @@ function typeOut(element, text, duration) {
     typeChar();
 }
 document.addEventListener('DOMContentLoaded', function() {
+    // Typing effect for welcome text
     const welcomeTextElement = document.getElementById('welcomeText');
     typeOut(welcomeTextElement, "Welcome Home", 100);
+
+    // Update footer year
+    const year = new Date().getFullYear();
+    document.querySelector(".u-footer").textContent = `©${year} God's Family Church`;
 });
+
